ca6ab30bfd7211a20d3e9fdf235a5545c0936cfb
Felix Berger <felix.enok.berger@gmail.com>
2025-09-26 17:19:59 +0000

    Refactor version checking logic in APRaidUtils to simplify data access and improve readability

--- main.lua
+++ main.lua
@@ -9,8 +9,8 @@ function APRaidUtils:OnInitialize()
     self:RegisterEvent("PLAYER_LOGIN", "OnPlayerLogin")
     
     Comms:RegisterCallback("CHECK_UPDATE", function(event, sender, distribution, data)
-        local theirVersion = data and data[1] and data[1].versions and data[1].versions.APRaidUtils
-        local myVersion = GetAddOnMetadata("APRaidUtils", "Version")
+        local theirVersion = data and data.versions and data.versions.APRaidUtils
+        local myVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
         if theirVersion and myVersion and IsVersionNewer(theirVersion, myVersion) then
             APRaidUtils:Print("A newer version of APRaidUtils is available: " .. theirVersion)
         end
@@ -18,21 +18,22 @@ function APRaidUtils:OnInitialize()
 end
 
 function APRaidUtils:OnPlayerLogin()
+    self:Print("Player logged in")
     if IsInGuild() then
         local myVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
-        if IsInGuild() and myVersion ~= "@project-version@" then
-            Comms:Broadcast("CHECK_UPDATE", "GUILD", {versions = {APRaidUtils = myVersion}})
-        end
+        Comms:Broadcast("CHECK_UPDATE", "GUILD", {versions = {APRaidUtils = myVersion}})
     end
 end
 
 function IsVersionNewer(their, mine)
-    local function parse(ver)
-        local major, minor, patch = string.match(ver or "", "(%d+)%.(%d+)%.(%d+)")
-        return tonumber(major) or 0, tonumber(minor) or 0, tonumber(patch) or 0
-    end
-    local maj1, min1, pat1 = parse(their)
-    local maj2, min2, pat2 = parse(mine)
+    local maj1, min1, pat1 = tostring(their):match("^v?(%d+)%.?(%d*)%.?(%d*)$")
+    maj1, min1, pat1 = tonumber(maj1) or 0, tonumber(min1) or 0, tonumber(pat1) or 0
+
+    -- Parse "mine"
+    local maj2, min2, pat2 = tostring(mine):match("^v?(%d+)%.?(%d*)%.?(%d*)$")
+    maj2, min2, pat2 = tonumber(maj2) or 0, tonumber(min2) or 0, tonumber(pat2) or 0
+
+    -- Compare
     if maj1 > maj2 then return true end
     if maj1 == maj2 and min1 > min2 then return true end
     if maj1 == maj2 and min1 == min2 and pat1 > pat2 then return true end
