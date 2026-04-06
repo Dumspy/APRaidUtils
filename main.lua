11673b844a223bb76750b841c20a0566b0202a97
Felix Berger <felix.enok.berger@gmail.com>
2026-04-07 00:34:46 +0200

    fix: suppress update notifications for dev builds
    Skip version check broadcast and update notifications when running from
    source (version equals @project-version@ placeholder).

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ APRaidUtils = LibStub("AceAddon-3.0"):NewAddon("APRaidUtils", "AceConsole-3.0", 
 local AceDB = LibStub("AceDB-3.0")
 local Comms = nil
 
+local addonVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
+local IsDevVersion = (addonVersion == "@project-version@")
+
 local defaults = {
     profile = {
     },
@@ -86,13 +89,15 @@ function APRaidUtils:OnInitialize()
     self:Print("Loaded")
     self:RegisterEvent("PLAYER_LOGIN", "OnPlayerLogin")
     
-    Comms:RegisterCallback("CHECK_UPDATE", function(event, sender, distribution, data)
-        local theirVersion = data and data.versions and data.versions.APRaidUtils
-        local myVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
-        if theirVersion and myVersion and IsVersionNewer(theirVersion, myVersion) then
-            APRaidUtils:Print("A newer version of APRaidUtils is available: " .. theirVersion)
-        end
-    end)
+    if not IsDevVersion then
+        Comms:RegisterCallback("CHECK_UPDATE", function(event, sender, distribution, data)
+            local theirVersion = data and data.versions and data.versions.APRaidUtils
+            local myVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
+            if theirVersion and myVersion and IsVersionNewer(theirVersion, myVersion) then
+                APRaidUtils:Print("A newer version of APRaidUtils is available: " .. theirVersion)
+            end
+        end)
+    end
 end
 
 function APRaidUtils:OnPlayerLogin()
@@ -100,7 +105,7 @@ function APRaidUtils:OnPlayerLogin()
     self:RegisterCurrentCharacter()
     self:NotifyOptionsChanged()
 
-    if IsInGuild() then
+    if IsInGuild() and not IsDevVersion then
         local myVersion = C_AddOns.GetAddOnMetadata("APRaidUtils", "Version")
         Comms:Broadcast("CHECK_UPDATE", "GUILD", {versions = {APRaidUtils = myVersion}})
     end
