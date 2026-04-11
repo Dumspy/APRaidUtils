03de661631cb0c34ff38befdac843dcd12cb97af
Felix Berger <felix.enok.berger@gmail.com>
2026-04-11 17:50:41 +0000

    chore: simplify flake.nix to match auxarmory style
    - Use flake-utils for system iteration
    - Remove fd, git, ripgrep, nixd (available in system)
    - Update to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,43 +2,29 @@
   description = "APRaidUtils Lua tooling shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in {
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              fd
-              git
               lua-language-server
               lua5_1
               lua51Packages.luacheck
-              nixd
-              ripgrep
               stylua
             ];
-
-            shellHook = ''
-              echo "APRaidUtils dev shell"
-              echo "  lua-language-server  # LSP server"
-              echo "  luac -p <file.lua>   # syntax check"
-              echo "  luacheck <paths>     # lint first-party Lua"
-              echo "  stylua <paths>       # format Lua"
-            '';
           };
-        });
-    };
+        };
+      }
+    );
 }
